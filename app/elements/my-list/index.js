06d415dfe84d4a0c0e57ba130c09b0038109d478
qiu shi <qius416@gmail.com>
2016-02-06 14:49:20 +0900

    init

---
+++ app/elements/my-list/index.js
@@ -0,0 +1,23 @@
+(() =>
+  Polymer({
+    is: 'my-list',
+
+    properties: {
+      items: {
+        type: Array,
+        notify: true,
+      }
+    },
+
+    ready: function() {
+      console.log('test');
+      this.items = [
+        'Responsive Web App boilerplate',
+        'Iron Elements and Paper Elements',
+        'End-to-end Build Tooling (including Vulcanize)',
+        'Unit testing with Web Component Tester',
+        'Routing with Page.js',
+        'Offline support with the Platinum Service Worker Elements'
+      ];
+    }
+  }))();
